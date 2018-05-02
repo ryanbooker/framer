912eaff23a8ba690043ca77415ab9b2e3b7af0a8
Ryan Booker <ryanbooker@gmail.com>
2018-05-02 15:44:35 +1000

    Command line options for filter and output type. Speed up filtering

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Framer
 
-Pick the best frame from every second of video, for an extremely naive definition of best. There are two filtering options available. By default the script uses image size as a proxy for clarity. The `--edges` option calculates image clarity using edge detection, and is an order of magnitude slower than `--size`.
+Pick the best frame from every second of video, for an extremely naive definition of best. There are two filtering options available. By default the script uses edge detection, however when using compressed image types, you may want to use image size as a fast a dirty approiximation for clarity.
 
 ```
-./framer.sh input.mov # generate frames using image size
-./framer.sh --size input.mov # generate frames using image size
-./framer.sh --edges input.mov # generate frames using image edges
+$ ./framer.sh
+Usage: framer.sh [-f <edges|size>] [-o <jp[e]g|p[i]ng|tif[f]>] -i <input_file>
+
+$ ./framer.sh -i input.mov # generate tiff frames using image edges
+$ ./framer.sh -f edges -o tif -i input.mov # generate tiff frames using image edges
+$ ./framer.sh -f size -o jpg -i input.mov # generate jpeg frames using image size
+$ ./framer.sh -f size -o png -i input.mov # generate ping frames using image size
 ```
 
 ## Dependencies
--- framer.sh
+++ framer.sh
@@ -6,24 +6,37 @@ trap '
   pkill -P $$
 ' INT
 
-trap 'rm -rf /tmp/frames' EXIT
+trap '
+  rm -rf /tmp/frames
+' EXIT
 
-# $1: filter, $2: input
+# $1: filter, $2: input file, $3: output args, $4 output type
 function generate() {
+  local duration=`ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 $2`
+  local fps=`ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of default=noprint_wrappers=1:nokey=1 $2 | bc -l | xargs printf "%.0f"`
+  seq 0 $duration | parallel frame "$1" "$2" "\"$3\"" "$4" "$fps" {1}
+}
+
+# $1: filter, $2: input file, $3: output args, $4 output type, $5 fps, $6: start
+function frame() {
   mkdir -p "frames"
-  mkdir -p "/tmp/frames"
+  mkdir -p "/tmp/frames/$6"
 
-  # Generate all the frames
-  ffmpeg -loglevel quiet -i "$2" -q:v 1 "/tmp/frames/%06d.jpg"
+  # Generate all the frames for this second
+  local start_number=`echo "1+$5*$6" | bc`
+  ffmpeg -loglevel quiet -y -i "$2" -ss "$6" -t 1 -start_number "$start_number" -an $3 "/tmp/frames/$6/%06d.$4"
 
-  # Pick from every 30
-  ls /tmp/frames/*.jpg | xargs -n30 | parallel $1 "frames" {1}
+  # Pick a frame
+  $1 "frames" "`ls /tmp/frames/$6/*.tif`"
+
+  # Clean up
+  rm -rf "/tmp/frames/$6"
 }
 
 # $1: destination, $2: input
 function filter_edges() {
   # Find edges
-  printf "%s\n" $2 | parallel 'convert "{1}" -colorspace Gray -edge 2 "{1}.edge.jpg"; identify -format "%[mean]+%[standard-deviation]" "{1}.edge.jpg" | { read a; echo "$a" | bc; } | xargs printf "%s,{1}\n"' | sort -nr | head -n 1 | tr "," "\n" | tail -n 1 | { read f; mv "$f" "$1"; }
+  parallel 'convert "{1}" -colorspace Gray -edge 2 "{1}.edge.jpg"; identify -format "%[mean]+%[standard-deviation]" "{1}.edge.jpg" | { read a; echo "$a" | bc; } | xargs printf "%s,{1}\n"' ::: $2 | sort -nr | head -n 1 | tr "," "\n" | tail -n 1 | { read f; mv "$f" "$1"; }
 }
 
 # $1: destination, $2: input
@@ -42,19 +55,68 @@ function spinner() {
   done
 }
 
-export -f filter_size
+function usage() { echo "Usage: `basename $0` [-f <edges|size>] [-o <jp[e]g|p[i]ng|tif[f]>] -i <input_file>" 1>&2; exit 1; }
+
+export -f frame
 export -f filter_edges
+export -f filter_size
 
-# Do it
-if [ "$1" = "--edges" ]; then
-  echo -en "-- Generating edge filtered frames...  "
-  generate filter_edges $2 & spinner
-elif [ "$1" = "--size" ]; then
-  echo -en "-- Generating size filtered frames...  "
-  generate filter_size $2 & spinner
+fitler=filter_edges
+type="tif"
+args="-pix_fmt rgb24 -vcodec tiff"
+
+while getopts ":f:o:i:" o; do
+  if [[ $OPTARG = -* ]]; then
+    OPTARG=$o
+    o=":"
+  fi
+
+  case "$o" in
+    i) input="$OPTARG";;
+    f)
+      case "$OPTARG" in
+        edge|edges)
+          filter=filter_edges
+          ;;
+        size)
+          filter=filter_size
+          ;;
+        *)
+          usage
+          exit $E_OPTERROR
+          ;;
+      esac
+      ;;
+    o)
+      case "$OPTARG" in
+        jpg|jpeg)
+          type="jpg"
+          args="-q:v 1"
+          ;;
+        png|ping)
+          type="png"
+          args=""
+          ;;
+        tif|tiff|*)
+          type="tif"
+          args="-pix_fmt rgb24 -vcodec tiff"
+          ;;
+      esac
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument. See usage." >&2
+      usage
+      exit $E_OPTERROR;
+      ;;
+    \?) usage;;
+  esac
+done
+
+if [ ! "$input" ]; then
+  usage
+  exit $E_OPTERROR
 else
-  echo -en "-- Generating size filtered frames...  "
-  generate filter_size $1 & spinner
+  echo -en "-- Generating edge filtered frames...  "
+  generate "$filter" "$input" "$args" "$type" & spinner
+  echo -e "\n-- Done."
 fi
-
-echo -e "\n-- Done."
