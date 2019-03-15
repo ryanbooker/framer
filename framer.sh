a576d691750f648d8f9ea73cb0baca460e54158c
Ryan Booker <ryanbooker@gmail.com>
2019-03-15 12:06:13 +1100

    Fix some linter warnings

--- framer.sh
+++ framer.sh
@@ -12,9 +12,11 @@ trap '
 
 # $1: filter, $2: input file, $3: output path, $4 output type, $5 output args
 function generate() {
-  local duration=`ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 "$2"`
-  local fps=`ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of default=noprint_wrappers=1:nokey=1 "$2" | bc -l | xargs printf "%.0f"`
-  seq 0 $duration | parallel frame "$1" "\"$2\"" "\"$3\"" "$4" "\"$5\"" "$fps" {1}
+  local duration
+  local fps
+  duration=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 "$2")
+  fps=$(ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of default=noprint_wrappers=1:nokey=1 "$2" | bc -l | xargs printf "%.0f")
+  seq 0 "$duration" | parallel frame "$1" "\"$2\"" "\"$3\"" "$4" "\"$5\"" "$fps" "{1}"
 }
 
 # $1: filter, $2: input file, $3 output path, $4 output type, $5: output args, $6 fps, $7: second to detect
@@ -26,11 +28,12 @@ function frame() {
   mkdir -p "$outdir"
 
   # Generate all the frames for this second
-  local start_number=`echo "1+$6*$7" | bc`
+  local start_number
+  start_number=$(echo "1+$6*$7" | bc)
   ffmpeg -loglevel quiet -y -i "$2" -ss "$7" -t 1 -start_number "$start_number" -an $5 "$temp/%06d.$4"
 
   # Pick a frame
-  frames=$(ls "$temp"/*.$4)
+  frames=$(ls "$temp"/*."$4")
   $1 "$outdir" "$frames"
 
   # Clean up
@@ -40,12 +43,12 @@ function frame() {
 # $1: destination, $2: input
 function filter_edges() {
   # Find edges
-  parallel 'convert {1} -colorspace Gray -edge 2 {1}.edge.png; identify -format "%[mean]+%[standard-deviation]" {1}.edge.png | { read a; echo "$a" | bc; } | xargs printf "%s,{1}\n"' ::: "$2" | sort -nr | head -n 1 | tr "," "\n" | tail -n 1 | tr -d "'" | { read f; mv "$f" "$1"; }
+  parallel 'convert {1} -colorspace Gray -edge 2 {1}.edge.png; identify -format "%[mean]+%[standard-deviation]" {1}.edge.png | { read a; echo "$a" | bc; } | xargs printf "%s,{1}\n"' ::: "$2" | sort -nr | head -n 1 | tr "," "\n" | tail -n 1 | tr -d "'" | { read -r f; mv "$f" "$1"; }
 }
 
 # $1: destination, $2: input
 function filter_size() {
-  stat -f %z,%N "$2" | sort -nr | head -n 1 | tr "," "\n" | tail -n 1 | tr -d "'" | { read f; mv "$f" "$1"; }
+  stat -f %z,%N "$2" | sort -nr | head -n 1 | tr "," "\n" | tail -n 1 | tr -d "'" | { read -r f; mv "$f" "$1"; }
 }
 
 function spinner() {
@@ -59,7 +62,7 @@ function spinner() {
   done
 }
 
-function usage() { echo "Usage: `basename $0` [-f <edges|size>] [-o <output_path>] [-t <jp[e]g|p[i]ng|tif[f]>] -i <input_file>" 1>&2; exit 1; }
+function usage() { echo "Usage: $(basename "$0") [-f <edges|size>] [-o <output_path>] [-t <jp[e]g|p[i]ng|tif[f]>] -i <input_file>" 1>&2; exit 1; }
 
 export -f frame
 export -f filter_edges
@@ -71,8 +74,8 @@ output="."
 args="-pix_fmt rgb24 -vcodec tiff"
 
 while getopts ":f:o:t:i:" o; do
-  if [[ $OPTARG = -* ]]; then
-    OPTARG=$o
+  if [[ "$OPTARG" = -* ]]; then
+    OPTARG="$o"
     o=":"
   fi
 
@@ -89,7 +92,7 @@ while getopts ":f:o:t:i:" o; do
           ;;
         *)
           usage
-          exit $E_OPTERROR
+          exit "$E_OPTERROR"
           ;;
       esac
       ;;
@@ -112,7 +115,7 @@ while getopts ":f:o:t:i:" o; do
     :)
       echo "Option -$OPTARG requires an argument. See usage." >&2
       usage
-      exit $E_OPTERROR;
+      exit "$E_OPTERROR";
       ;;
     \?) usage;;
   esac
@@ -120,7 +123,7 @@ done
 
 if [ ! "$input" ]; then
   usage
-  exit $E_OPTERROR
+  exit "$E_OPTERROR"
 else
   echo -en "-- Generating edge filtered frames...  "
   generate "$filter" "$input" "$output" "$type" "$args" & spinner
