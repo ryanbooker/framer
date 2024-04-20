5f78851a08ef6a3e31cbc77ba134a93f42d48ce5
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2024-04-20 12:39:51 +1000

    Add -j option for specifying parallel -j job count

--- framer.sh
+++ framer.sh
@@ -10,7 +10,13 @@ trap '
   rm -rf /tmp/frames
 ' EXIT
 
-# $1: filter, $2: input file, $3: output path, $4 output type, $5 frames per second, $6 output args
+# $1: filter
+# $2: input file
+# $3: output path
+# $4 output type
+# $5 frames per second
+# $6 output args
+# $7 max parallel jobs
 function generate() {
   local duration
   local fps
@@ -22,10 +28,18 @@ function generate() {
 
   step=$(printf '%f\n' "$(echo "1/$5" | bc -l)")
 
-  seq 0 "$step" "$duration" | parallel -j 0 frame "$1" "\"$2\"" "\"$3\"" "$4" "\"$6\"" "$fps" "$step" "{1}"
+  seq 0 "$step" "$duration" | parallel -j "$7" frame "$1" "\"$2\"" "\"$3\"" "$4" "\"$6\"" "$fps" "$step" "{1}" "$7"
 }
 
-# $1: filter, $2: input file, $3 output path, $4 output type, $5: output args, $6 fps, $7: step, $8: segment to detect
+# $1: filter
+# $2: input file
+# $3 output path
+# $4 output type
+# $5: output args
+# $6 fps
+# $7: step
+# $8: segment to detect
+# $9: max parallel jobs
 function frame() {
   temp="/tmp/frames/$(basename "$3")/$8"
   mkdir -p "$temp"
@@ -44,31 +58,37 @@ function frame() {
 
   # Pick a frame
   frames=$(ls "$temp"/*."$4")
-  $1 "$outdir" "$frames"
+  $1 "$outdir" "$frames" "$9"
 
   # Clean up
   rm -rf "$temp"
 }
 
-# $1: destination, $2: input
+# $1: destination
+# $2: input file list
+# $3: max parallel jobs
 function filter_edges() {
   # Find edges
-  parallel 'convert {1} -colorspace Gray -edge 1 {1}.edge.png; identify -format "%[standard-deviation]" {1}.edge.png | { read a; echo "$a" | bc; } | xargs printf "%s,{1}\n"' ::: "$2" | sort -nr | head -n 1 | cut -d ',' -f2 | tr -d "'" | {
+  parallel -j "$3" 'convert {1} -colorspace Gray -edge 1 {1}.edge.png; identify -format "%[standard-deviation]" {1}.edge.png | { read a; echo "$a" | bc; } | xargs printf "%s,{1}\n"' ::: "$2" | sort -nr | head -n 1 | cut -d ',' -f2 | tr -d "'" | {
     read -r f
     mv "$f" "$1"
   }
 }
 
-# $1: destination, $2: input
+# $1: destination
+# $2: input file list
+# $3: max parallel jobs
 function filter_canny() {
   # Find edges
-  parallel 'convert {1} -colorspace Gray -canny 0x1+10%+30% {1}.edge.png; identify -format "%[standard-deviation]" {1}.edge.png | { read a; echo "$a" | bc; } | xargs printf "%s,{1}\n"' ::: "$2" | sort -nr | head -n 1 | cut -d ',' -f2 | tr -d "'" | {
+  parallel -j "$3" 'convert {1} -colorspace Gray -canny 0x1+10%+30% {1}.edge.png; identify -format "%[standard-deviation]" {1}.edge.png | { read a; echo "$a" | bc; } | xargs printf "%s,{1}\n"' ::: "$2" | sort -nr | head -n 1 | cut -d ',' -f2 | tr -d "'" | {
     read -r f
     mv "$f" "$1"
   }
 }
 
-# $1: destination, $2: input
+# $1: destination
+# $2: input file list
+# $3: max parallel jobs
 function filter_size() {
   stat -c %s,%N $2 | sort -nr | head -n 1 | cut -d ',' -f2 | tr -d "'" | {
     read -r f
@@ -88,7 +108,14 @@ function spinner() {
 }
 
 function usage() {
-  echo "Usage: $(basename "$0") [-f <canny|edges|size>] [-o <output_path>] [-t <jp[e]g|p[i]ng|tif[f]>] -i <input_file> -n <frames_per_second>" 1>&2
+  echo "Usage: $(basename "$0") [-f <canny|edges|size>] [-o <output_path>] [-t <jp[e]g|p[i]ng|tif[f]>] -i <input_file> -n <frames_per_second> -j <max_parallel_jobs>" 1>&2
+  echo "Parameters:"
+  echo "  -f filtering method, defaults to 'canny'"
+  echo "  -o output path, a subfolder 'frames' will be created here"
+  echo "  -t output format, defaults to 'tiff'"
+  echo "  -i input file path"
+  echo "  -n frame per second of input, defaults to 1"
+  echo "  -j maximum parallel job count, defaults to 20% of cpu cores, passing 0 will be eat all your resources. It is passed directly to GNU parallel's -j option"
   exit 1
 }
 
@@ -102,8 +129,9 @@ type="png"
 output="."
 args="-pix_fmt rgb24 -vcodec tiff"
 number=1
+maxjobs="20%"
 
-while getopts ":f:o:t:i:n:" o; do
+while getopts ":f:o:t:i:n:j:" o; do
   if [[ "$OPTARG" = -* ]]; then
     OPTARG="$o"
     o=":"
@@ -151,6 +179,9 @@ while getopts ":f:o:t:i:n:" o; do
     n)
       number="$OPTARG"
       ;;
+    j)
+      maxjobs="$OPTARG"
+      ;;
     :)
       echo "Option -$OPTARG requires an argument. See usage." >&2
       usage
@@ -165,6 +196,6 @@ if [ ! "$input" ]; then
   usage
 else
   echo -en "-- Generating filtered frames...  "
-  generate "$filter" "$input" "$output" "$type" "$number" "$args" & spinner
+  generate "$filter" "$input" "$output" "$type" "$number" "$args" "$maxjobs" & spinner
   echo -e "\n-- Done."
 fi
